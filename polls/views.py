985c2a404f7d91fea92ba3abf42c3704a5b71b8b
songchenglin <songchenglin92@gmail.com>
2014-06-23 22:30:36 +0800

    Modify view show first to fifth Question in url polls.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello,Songchenglin add here!!!!.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 def detail(request, question_id):
     response = 'You\'re looking at the question %s!!!!!'
     return HttpResponse(response % question_id)
