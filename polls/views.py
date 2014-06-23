9dbd6e919cedb2faee0b9f7445ca331cdf03fbaa
songchenglin <songchenglin92@gmail.com>
2014-06-23 23:17:37 +0800

    Add template file for polls,show questions in polls by index.html

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.template import loader
 from polls.models import Question
 
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ','.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.all().order_by('question_text')
+    context =  {
+        'latest_question_list':latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
 def detail(request, question_id):
     response = 'You\'re looking at the question %s!!!!!'
     return HttpResponse(response % question_id)
